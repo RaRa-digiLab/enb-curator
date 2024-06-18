0cfce24e84faed9cc43468fa4efb838169c90659
Krister Kruusmaa <73342507+krkryger@users.noreply.github.com>
2024-06-18 19:03:21 +0300

    add loop for all books, elapsed time

--- main.py
+++ main.py
@@ -1,15 +1,57 @@
 from src.python.harvest import harvest_oai, collections
 from src.python.convert import oai_to_dataframe
-
+from src.python.clean import clean_dataframe, organize_columns
+from datetime import timedelta
+import time
 import sys
 
-key = sys.argv[1]
+if __name__ == "__main__":
+    start_time = time.time()
+    key = sys.argv[1]
 
-print(f"Harvesting {collections[key]['title']}")
-harvest_oai(key=key, savepath=f"data/raw/{key}.xml")
+    if key == "erb_all_books":
+        for k in ["erb_books", "erb_non_estonian"]:
+            # harvest and save the raw XML file
+            print(f"\nHarvesting {collections[k]['title']}")
+            harvest_oai(key=k, savepath=f"data/raw/{k}.xml")
 
-print(f"Converting {key} to dataframe")
-df = oai_to_dataframe(f"data/raw/{key}.xml", marc_threshold=0.05, replace_columns=False)
-df.to_parquet(f"data/converted/{key}.parquet")
+            # take the raw XML file, convert it to a dataframe and save it
+            print(f"\nConverting {k} to dataframe")
+            df = oai_to_dataframe(f"data/raw/{k}.xml", rename_columns=False)
+            df.to_parquet(f"data/converted/{k}.parquet")
+        
+        # concatenate the dataframes for cleaning
+        import pandas as pd
+        erb_est = pd.read_parquet("data/converted/erb_books.parquet")
+        erb_non = pd.read_parquet("data/converted/erb_non_estonian.parquet")
+        df = pd.concat([erb_est, erb_non])
+        del(erb_est, erb_non) # free up RAM
 
-print("Finished")
+        # clean and filter the converted dataframe
+        print("\nCleaning dataframe")
+        df = clean_dataframe(df)
+        df = organize_columns(df)
+        df.to_parquet(f"data/cleaned/{key}.parquet")
+        
+        
+    else:
+        # harvest and save the raw XML file
+        print(f"\nHarvesting {collections[key]['title']}")
+        harvest_oai(key=key, savepath=f"data/raw/{key}.xml")
+
+        # take the raw XML file, convert it to a dataframe and save it
+        print(f"\nConverting {key} to dataframe")
+        df = oai_to_dataframe(f"data/raw/{key}.xml", rename_columns=False)
+        df.to_parquet(f"data/converted/{key}.parquet")
+
+        # clean and filter the converted dataframe
+        print("\nCleaning dataframe")
+        df = clean_dataframe(df)
+        df = organize_columns(df)
+        df.to_parquet(f"data/cleaned/{key}.parquet")
+        # df.to_csv(f"data/cleaned/{key}.tsv", sep="\t", encoding="utf8", index=False)
+
+    end_time = time.time()
+    elapsed_time = end_time - start_time  # Calculate the elapsed time
+    formatted_time = str(timedelta(seconds=elapsed_time))  # Format the elapsed time
+    print(f"\nCompleted in {formatted_time}")
